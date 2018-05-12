13283f590d69d0266f5ef02b6d1fb50e13a68d61
Matthias Küblböck <matthias.kueblboeck@yahoo.de>
2018-05-12 18:58:43 +0200

    add/delete Arrows/Elements

--- client/src/app/services/diagram.service.ts
+++ client/src/app/services/diagram.service.ts
@@ -1,14 +1,15 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 
 import '../models/arrow.model';
 import '../models/device.model';
-import {ObserveOnMessage} from "rxjs/operators/observeOn";
-import {forEach} from "@angular/router/src/utils/collection";
+import { ObserveOnMessage } from 'rxjs/operators/observeOn';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Injectable()
 export class DiagramService {
 
   devices: Device<any>[] = [];
+  arrows: Arrow[] = [];
 
   constructor(/* TODO inject other services or core classes if necessary */) {
   }
@@ -21,7 +22,9 @@ export class DiagramService {
   }
 
   initArrows(func: (arrow: Arrow) => void): void {
-    // TODO execute func for each of the already existing arrows
+    this.arrows.forEach(arrow => {
+      func(arrow);
+    });
   }
 
   afterDeviceAdd(device: Device<any>): void {
@@ -30,7 +33,10 @@ export class DiagramService {
   }
 
   afterDeviceDelete(device: Device<any>): void {
-    // TODO remove the device from that list
+    const index = this.devices.indexOf(device, 0);
+    if (index > -1) {
+      this.devices.splice(index, 1);
+    }
   }
 
   onDeviceDetails(device: Device<any>): void {
@@ -38,10 +44,13 @@ export class DiagramService {
   }
 
   afterArrowAdd(arrow: Arrow): void {
-    // TODO add the arrow to some list
+    this.arrows.push(arrow);
   }
 
   afterArrowDelete(arrow: Arrow): void {
-    // TODO remove the arrow from that list
+    const index = this.arrows.indexOf(arrow, 0);
+    if (index > -1) {
+      this.arrows.splice(index, 1);
+    }
   }
 }
