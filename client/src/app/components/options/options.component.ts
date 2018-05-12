11d07d5d2014269964d11349258b45548b0dd19e
Matthias Küblböck <matthias.kueblboeck@yahoo.de>
2018-05-12 13:22:18 +0200

    options-page; changePassword

--- client/src/app/components/options/options.component.ts
+++ client/src/app/components/options/options.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { PasswordChangeRequest } from '../../models';
+import { AuthService } from '../../services/authservice';
 
 @Component({
   selector: 'app-options',
   templateUrl: './options.component.html'
 })
 export class OptionsComponent {
+  credentials: PasswordChangeRequest = { oldPassword: '', newPassword: '' };
+  showError = false;
+  newPassword;
+  showWrongPassword = false;
+  showSuccess = false;
+  constructor(private auth: AuthService) { }
 
+  saveSettings() {
+    console.log(this.newPassword + ' ' + this.credentials.newPassword);
+    if (this.newPassword === this.credentials.newPassword) {
+      this.auth.changePassword(this.credentials, access => {
+        if (access) {
+          this.showSuccess = true;
+          this.showError = false;
+          this.showWrongPassword = false;
+        } else {
+          this.showWrongPassword = true;
+        }
+      });
+    } else {
+      this.showError = true;
+    }
+  }
 }
--- client/src/app/services/authservice.ts
+++ client/src/app/services/authservice.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { AuthenticationRequest } from '../models';
+import { AuthenticationRequest, PasswordChangeRequest } from '../models';
 import { error } from 'util';
 
 @Injectable()
@@ -8,8 +8,7 @@ export class AuthService {
 
   httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': 'my-auth-token'
+      'Content-Type': 'application/json'
     })
   };
 
@@ -21,13 +20,29 @@ export class AuthService {
     console.log('Authservice login called');
     this.http.post('http://localhost:8081/api/login', credentials, this.httpOptions).
       subscribe(data => {
+        console.log(data);
         if (data === true) {
           callback(access = true);
         }
       }, response => {
+        console.log(response);
         if (response) {
           callback(access);
         }
       });
+  }
+
+  changePassword(credentials: PasswordChangeRequest, callback: (access) => void) {
+    console.log('changepassword called');
+    let access = false;
+    this.http.post('http://localhost:8081/api/password', credentials, this.httpOptions).subscribe(data => {
+      if (data === true) {
+        callback(access = true);
+      }
+    }, response => {
+      if (response) {
+        callback(access);
+      }
+    });
   }
 }
