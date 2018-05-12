1a9a4a17af94857c7181979ca8cabc0618bed497
Matthias Küblböck <matthias.kueblboeck@yahoo.de>
2018-05-12 18:16:16 +0200

    get Devices

--- client/src/app/components/overview/overview.component.ts
+++ client/src/app/components/overview/overview.component.ts
@@ -2,21 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { AvailableDevice } from '../../models/device.available';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
-import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html'
 })
 export class OverviewComponent implements OnInit {
-  devices: Observable<AvailableDevice[]>;
+  devices: AvailableDevice[];
 
   constructor(private http: HttpClient) {
-    this.devices = this.http.get<AvailableDevice[]>('http://localhost:8081/api/devices');
   }
 
   ngOnInit() {
-    this.devices = this.http.get<AvailableDevice[]>('http://localhost:8081/api/devices');
+    this.http.get<AvailableDevice[]>('http://localhost:8081/api/devices').subscribe(data => this.devices = data.devices);
     console.log(this.devices);
   }
 }
