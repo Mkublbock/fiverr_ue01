c44f2012fa9b9c41b59459d476fd00fb52f290c0
Matthias Küblböck <matthias.kueblboeck@yahoo.de>
2018-05-12 12:29:00 +0200

    login via server

--- client/src/app/components/app/app.component.ts
+++ client/src/app/components/app/app.component.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/authservice';
 import { Router } from '@angular/router';
+import { AuthenticationRequest } from '../../models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  credentials = { username: '', password: '' };
+  credentials: AuthenticationRequest = { username: '', password: '' };
   showError = false;
   constructor(private auth: AuthService, private router: Router) { }
 
   login() {
     console.log(' Login called ');
-    this.auth.login(this.credentials, validation => {
-      if (validation) {
-        //  GOTO OVERVIEW
-      } else {
+    this.auth.login(this.credentials, access => {
+      console.log(access);
+      if (access) {
+        this.showError = false;
+        this.router.navigate(['/overview']);
+      } else if (access === false) {
         this.showError = true;
       }
     });
--- client/src/app/services/authservice.ts
+++ client/src/app/services/authservice.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthenticationRequest } from '../models';
+import { error } from 'util';
 
 @Injectable()
 export class AuthService {
 
-  login(credentials, access) {
-    // TODO: IMPLEMENT REST CALL
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'my-auth-token'
+    })
+  };
+
+  constructor(private http: HttpClient) {
+  }
+
+  login(credentials: AuthenticationRequest, callback: (access) => void) {
+    let access = false;
+    console.log('Authservice login called');
+    this.http.post('http://localhost:8081/api/login', credentials, this.httpOptions).
+      subscribe(data => {
+        if (data === true) {
+          callback(access = true);
+        }
+      }, response => {
+        if (response) {
+          callback(access);
+        }
+      });
   }
 }
