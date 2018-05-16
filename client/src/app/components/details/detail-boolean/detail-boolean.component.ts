f64e44c29980d3cd9b01fc82047460635e007526
Stefan Haslhofer <haslhofer.stefan@gmail.com>
2018-05-16 17:37:21 +0200

    pie chart

--- client/src/app/components/details/detail-boolean/detail-boolean.component.ts
+++ client/src/app/components/details/detail-boolean/detail-boolean.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { DeviceService } from '../../../services/device.service';
-import { Observable } from 'rxjs/Observable';
-import { DiagramService } from '../../../services/diagram.service';
-import { AvailableDevice } from '../../../models/device.available';
-import { HttpClient } from '@angular/common/http';
-import { map, filter } from 'rxjs/operators';
-import { Router } from '@angular/router';
+import {Component, OnInit, Inject} from '@angular/core';
+import {DeviceService} from '../../../services/device.service';
+import {Observable} from 'rxjs';
+import {DiagramService} from '../../../services/diagram.service';
+import {AvailableDevice} from '../../../models/device.available';
+import {HttpClient} from '@angular/common/http';
+import {map, filter} from 'rxjs/operators';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-detail-boolean',
@@ -16,12 +16,30 @@ export class DetailBooleanComponent {
   devices: any[];
   device;
   index: string;
+  data: Object[] = [{"name":"true","value":0},{"name": "false", "value":0}];
+
+  trueCount: number = 0;
+  falseCount: number = 0;
+
+  textareaRow: string[] = [];
 
   constructor(private diagramServce: DiagramService, private http: HttpClient, private router: Router) {
     this.devices = diagramServce.devices;
     this.index = this.router.url.slice(this.router.url.length - 1, this.router.url.length);
     this.device = this.devices[parseInt(this.index, 10) - 1];
+    this.data = [{"name":"true","value":0},{"name": "false", "value":0}];
     console.log(this.device);
+  }
 
+  addData(checkBoxValue: boolean) {
+    if (checkBoxValue) {
+      this.trueCount++;
+      this.textareaRow.push("Aus -> An");
+    } else {
+      this.textareaRow.push("An -> Aus");
+      this.falseCount++;
+    }
+    console.log("asd");
+    this.data = [{"name":"true","value":this.trueCount},{"name": "false", "value":this.falseCount}];
   }
 }
