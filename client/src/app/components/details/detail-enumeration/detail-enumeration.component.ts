d8cb7fd20e1036f8a20cfc6075616034dbd001e6
Stefan Haslhofer <haslhofer.stefan@gmail.com>
2018-05-17 14:07:09 +0200

    charts fertig oida

--- client/src/app/components/details/detail-enumeration/detail-enumeration.component.ts
+++ client/src/app/components/details/detail-enumeration/detail-enumeration.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { DiagramService } from '../../../services/diagram.service';
-import { AvailableDevice } from '../../../models/device.available';
-import { HttpClient } from '@angular/common/http';
-import { map, filter } from 'rxjs/operators';
-import { Router } from '@angular/router';
+import {Component, OnInit, Inject} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {DiagramService} from '../../../services/diagram.service';
+import {AvailableDevice} from '../../../models/device.available';
+import {HttpClient} from '@angular/common/http';
+import {map, filter} from 'rxjs/operators';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-detail-enumeration',
-  templateUrl: './detail-enumeration.component.html',
-  styleUrls: ['./detail-enumeration.component.css']
+  templateUrl: './detail-enumeration.component.html'
 })
 export class DetailEnumerationComponent {
 
@@ -19,12 +18,13 @@ export class DetailEnumerationComponent {
   selectValue;
   data: Object[];
 
-  trueCount = 0;
-  falseCount = 0;
-
   devices: any[];
   device;
   index: string;
+
+  finishedCount: number = 0;
+  unfinishedCount: number = 0;
+  nearlyFinCount: number = 0;
 
   constructor(private diagramServce: DiagramService, private http: HttpClient, private router: Router) {
     this.devices = diagramServce.devices;
@@ -34,15 +34,29 @@ export class DetailEnumerationComponent {
   }
 
   addData(value) {
-    console.log(value);
     this.timeStamp = new Date();
     const time = this.timeStamp.getDate() + '.' + this.timeStamp.getMonth() + '.' + this.timeStamp.getFullYear()
       + ', ' + this.timeStamp.getHours() + ':' + this.timeStamp.getMinutes() + ':' + this.timeStamp.getSeconds() + ': ';
     if (this.lastValue !== value) {
       this.textareaRow += time + '' + this.lastValue + '  ->  ' + value + '\n';
       this.lastValue = value;
+
+      //check the dropdown value and increment the values
+      if (value === "Fertig") {
+        this.finishedCount++;
+      } else {
+        if (value === "Unfertig") {
+          this.unfinishedCount++;
+        } else {
+          this.nearlyFinCount++;
+        }
+      }
+
+      this.data = [{"name": "Fertig", "value": this.finishedCount}, {
+        "name": "Unfertig",
+        "value": this.unfinishedCount
+      }, {"name": "Teilweise Fertig", "value": this.nearlyFinCount}];
     }
-    this.data = [{ 'name': 'true', 'value': this.trueCount }, { 'name': 'false', 'value': this.falseCount }];
   }
 
 }
