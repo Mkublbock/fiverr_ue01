21d3e2ddfd2285adf6143290bea232fb57fcbb3c
Stefan Haslhofer <haslhofer.stefan@gmail.com>
2018-05-17 13:30:17 +0200

    Line chart

--- client/src/app/components/details/detail-continuous/detail-continuous.component.ts
+++ client/src/app/components/details/detail-continuous/detail-continuous.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { DiagramService } from '../../../services/diagram.service';
-import { AvailableDevice } from '../../../models/device.available';
-import { HttpClient } from '@angular/common/http';
-import { map, filter } from 'rxjs/operators';
-import { Router } from '@angular/router';
+import {Component, OnInit, Inject} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {DiagramService} from '../../../services/diagram.service';
+import {AvailableDevice} from '../../../models/device.available';
+import {HttpClient} from '@angular/common/http';
+import {map, filter} from 'rxjs/operators';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-detail-continuous',
@@ -16,18 +16,24 @@ export class DetailContinuousComponent {
   device;
   index: string;
 
-  data: Object[] = [];
+  data: any[] = [
+    {
+      "name": "Temparatur",
+      "series": [
+
+      ]
+    }
+  ];
 
   constructor(private diagramServce: DiagramService, private http: HttpClient, private router: Router) {
     this.devices = diagramServce.devices;
     this.index = this.router.url.slice(this.router.url.length - 1, this.router.url.length);
     this.device = this.devices[parseInt(this.index, 10) - 1];
-    console.log(this.device.yLabel);
   }
 
-  addData() {
-
-
-    this.data;
+  addData(value: number) {
+    //insert date at "Temparatur"
+    this.data[0].series.push({"name": "Temparatur"+value, "value": value});
+    this.data=[...this.data];
   }
 }
